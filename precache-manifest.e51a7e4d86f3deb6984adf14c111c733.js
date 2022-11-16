35c13f9bc5e6fc1ad77e0ae106a8a1285ec5fed1
CarlosAlvaZ <carlos.alvarenga017@gmail.com>
2022-11-16 16:43:22 -0600

    Updates

---
+++ precache-manifest.e51a7e4d86f3deb6984adf14c111c733.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4afe9897bf7eb695562e",
+    "url": "/gamestation/static/css/main.8387873b.chunk.css"
+  },
+  {
+    "revision": "4afe9897bf7eb695562e",
+    "url": "/gamestation/static/js/main.4afe9897.chunk.js"
+  },
+  {
+    "revision": "d5f1a4599f9506ac9601",
+    "url": "/gamestation/static/js/1.d5f1a459.chunk.js"
+  },
+  {
+    "revision": "4e40d68d1a22c2392856",
+    "url": "/gamestation/static/js/runtime~main.4e40d68d.js"
+  },
+  {
+    "revision": "ed839c088c0fc80eb527b1a6b9d1f449",
+    "url": "/gamestation/static/media/logo.ed839c08.png"
+  },
+  {
+    "revision": "f96253aa198755e2134bba52090204f3",
+    "url": "/gamestation/static/media/AcuminVariableConcept.f96253aa.otf"
+  },
+  {
+    "revision": "ba4860417d90cbdde40796ebd20493ed",
+    "url": "/gamestation/static/media/login-background.ba486041.jpg"
+  },
+  {
+    "revision": "841082ea80a89fcbc65b344728c06788",
+    "url": "/gamestation/index.html"
+  }
+];
